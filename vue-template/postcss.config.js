66f038ab39058bd5e1c2c1e87ea71f1db93160d3
1099216788@qq.com <1099216788@qq.com>
2020-06-08 17:08:52 +0800

    no message

--- vue-template/postcss.config.js
+++ vue-template/postcss.config.js
@@ -16,7 +16,7 @@ module.exports = {
       //viewportHeight: 1334, // (Number) The height of the viewport. Optional
       unitPrecision: 0, // (Number) The decimal numbers to allow the REM units to grow to.
       viewportUnit: 'vw', // (String) Expected units.
-      selectorBlackList: ['.ignore', '.hairlines', 'ignore-container'], // (Array) The selectors to ignore and leave as px.
+      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
       minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
       mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
     },
